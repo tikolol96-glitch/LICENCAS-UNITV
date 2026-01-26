88f241adebef1e8dc494b9876ec95f138606908e
Elieser Andrade <tikolol96@gmail.com>
2026-01-26 00:16:17 -0300

    coommit

--- app.py
+++ app.py
@@ -43,5 +43,5 @@ def status():
     })
 
 if __name__ == "__main__":
-    port = int(os.environ.get("PORT", 5000))
+    port = int(os.environ.get("PORT", 10000))
     app.run(host="0.0.0.0", port=port)
